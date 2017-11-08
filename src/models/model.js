e8c5322c3832058b9dab38ee60812a957aa10471
freemanchen <freemanxchen@gmail.com>
2017-11-08 09:49:35 -0800

    change familiarizeAndPrototype to Model method to use this.g.partition

--- src/models/model.js
+++ src/models/model.js
@@ -11,7 +11,7 @@ class Model {
         return;
       }
       // Create nicer Object
-      let response = familiarizeAndPrototype(result, childClass);
+      let response = this.familiarizeAndPrototype(result, childClass);
 
       callback(null, response);
     });
@@ -87,7 +87,7 @@ class Model {
   checkSchema(schema, props, checkRequired) {
     const schemaKeys = Object.keys(schema);
     const propsKeys = Object.keys(props);
-      
+
     if (checkRequired) {
       for (let i = 0; i < schemaKeys.length; i += 1) {
         let key = schemaKeys[i];
@@ -96,7 +96,7 @@ class Model {
         }
       }
     }
-    
+
     for (let i = 0; i < propsKeys.length; i += 1) {
       let key = propsKeys[i];
       if (!schemaKeys.includes(key)) return false;
@@ -106,33 +106,32 @@ class Model {
     }
     return true;
   }
-}
 
-
-const familiarizeAndPrototype = (gremlinResponse, childClass) => {
-  let data = [];
-  gremlinResponse.forEach((grem) => {
-    let object = Object.create(childClass);
-    object.id = grem.id;
-    object.label = grem.label;
-    if (childClass.constructor.name === 'EdgeModel') {
-      object.inV = grem.inV;
-      object.outV = grem.outV
-    }
-
-    let currentPartition = this.g.partition ? this.g.partition : '';
-    Object.keys(grem.properties).forEach((propKey) => {
-      if (propKey != currentPartition) {
-        if (childClass.constructor.name === 'EdgeModel') {
-          object[propKey] = grem.properties[propKey];
-        } else {
-          object[propKey] = grem.properties[propKey][0].value;
-        }
+  familiarizeAndPrototype(gremlinResponse, childClass) {
+    let data = [];
+    gremlinResponse.forEach((grem) => {
+      let object = Object.create(childClass);
+      object.id = grem.id;
+      object.label = grem.label;
+      if (childClass.constructor.name === 'EdgeModel') {
+        object.inV = grem.inV;
+        object.outV = grem.outV
       }
-    });
-    data.push(object);
-  })
-  return data;
+
+      let currentPartition = this.g.partition ? this.g.partition : '';
+      Object.keys(grem.properties).forEach((propKey) => {
+        if (propKey != currentPartition) {
+          if (childClass.constructor.name === 'EdgeModel') {
+            object[propKey] = grem.properties[propKey];
+          } else {
+            object[propKey] = grem.properties[propKey][0].value;
+          }
+        }
+      });
+      data.push(object);
+    })
+    return data;
+  }
 }
 
 module.exports = Model;
