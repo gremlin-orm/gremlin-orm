7471f9050a07f647e5bcfc768d1d64231b67909d
Gordon <godronus@yahoo.com>
2017-11-08 10:27:41 -0800

    createE is back

--- src/models/vertex-model.js
+++ src/models/vertex-model.js
@@ -21,6 +21,25 @@ class VertexModel extends Model {
     this.executeQuery(gremlinStr, this, callback);
   }
 
+  createE(edge, props, vertex, callback) {
+    let outGremlinStr = this.getGremlinStr();
+    if (outGremlinStr === '') {
+      callback({'error': 'Gremlin Query has not been initialised for out Vertex'});
+    }
+    let inGremlinStr = vertex.getGremlinStr();
+    if (inGremlinStr === '') {
+      callback({'error': 'Gremlin Query has not been initialised for in Vertex'});
+    }
+    // const schemaCheck = this.checkSchema(edge.schema, props, true);
+    // if (Object.keys(schemaCheck).length === 0) {
+    //   callback({'error': schemaCheck});
+    // }
+    let gremlinQuery = outGremlinStr + `.addE('${edge.label}')${this.actionBuilder('property', props)}.to(` + 
+                                        inGremlinStr + ")";
+                                        
+    executeOrPass(gremlinQuery, this, callback);
+  }
+
   find(props, callback) {
     let gremlinStr = 'g.V()' + this.actionBuilder('has', props);
     this.executeOrPass(gremlinStr, this, callback);
