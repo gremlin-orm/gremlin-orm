054595bee12c5c7469c2a774e80002873f66bfea
freemanchen <freemanxchen@gmail.com>
2017-11-07 22:06:59 -0800

    fix executeOrPass and change parentClass to childClass

--- src/models/model.js
+++ src/models/model.js
@@ -4,23 +4,23 @@ class Model {
     this.gremlinStr = gremlinStr;
   }
 
-  executeQuery(query, parentClass, callback) {
-    parentClass.g.client.execute(query, (err, result) => {
+  executeQuery(query, childClass, callback) {
+    this.g.client.execute(query, (err, result) => {
       if (err) {
         callback({'error': err});
         return;
       }
       // Create nicer Object
-      let response = familiarizeAndPrototype(result, parentClass);
+      let response = familiarizeAndPrototype(result, childClass);
 
       callback(null, response);
     });
   }
 
-  executeOrPass(gremlinStr, this, callback) {
-    if (callback) this.executeQuery(gremlinStr, this, callback);
+  executeOrPass(gremlinStr, childClass, callback) {
+    if (callback) this.executeQuery(gremlinStr, childClass, callback);
     else {
-      let response = Object.create(this);
+      let response = Object.create(childClass);
       response.gremlinStr = gremlinStr;
       return response;
     }
@@ -106,23 +106,20 @@ class Model {
   }
 }
 
-const familiarizeAndPrototype = (gremlinResponse, parentClass) => {
-    console.log("parentClass", parentClass);
-    console.log("gremlinResponse", gremlinResponse);
-  parentClass = parentClass || this; // What can this ever be?
+const familiarizeAndPrototype = (gremlinResponse, childClass) => {
   let data = [];
   gremlinResponse.forEach((grem) => {
-    let object = Object.create(parentClass);
+    let object = Object.create(childClass);
     object.id = grem.id;
     object.label = grem.label;
-    if (parentClass.constructor.name === 'EdgeModel') {
+    if (childClass.constructor.name === 'EdgeModel') {
       object.inV = grem.inV;
       object.outV = grem.outV
     }
-    let currentPartition = parentClass.partition ? parentClass.partition : '';
+    let currentPartition = this.g.partition ? this.g.partition : '';
     Object.keys(grem.properties).forEach((propKey) => {
       if (propKey != currentPartition) {
-        if (parentClass.constructor.name === 'EdgeModel') {
+        if (childClass.constructor.name === 'EdgeModel') {
           object[propKey] = grem.properties[propKey];
         } else {
           object[propKey] = grem.properties[propKey][0].value;
