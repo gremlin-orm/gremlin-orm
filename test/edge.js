67b0bf34503801dcad0be1c2daf8ffc57f73cdc5
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-11-15 13:58:40 -0800

    add tests

--- test/edge.js
+++ test/edge.js
@@ -26,10 +26,16 @@ const Knows = g.defineEdge('knows', {
   }
 });
 
+let john;
+let bob;
+
 describe('Edge Model', () => {
   beforeEach(done => {
-    g.queryRaw('g.V().drop()', () => {done()});
+    g.queryRaw('g.V().drop()', () => {
+      done();
+    });
   });
+  
   describe('Define', () => {
     it('Should define a new edge model called Knows', () => {
       expect(Knows.create).to.be.a('function');
@@ -37,11 +43,11 @@ describe('Edge Model', () => {
   });
 
   describe('Create', () => {
-    it('Should create a new edge with valid parameters', (done) => {
+    it('Should create a new edge with valid properties', (done) => {
       Person.create({'name': 'John', 'age': 18, 'dob': '12/18/1999', developer: true}, (err, result) => {
-        let john = result;
+        john = result;
         Person.create({'name': 'Bob', 'age': 23, 'dob': '04/30/1994', developer: false}, (err, result) => {
-          let bob = result;
+          bob = result;
           Knows.create(john, bob, {duration: 5}, (err, result) => {
             expect(result).to.have.property('id');
             expect(result.label).to.equal('knows');
