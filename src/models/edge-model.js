77f31d5edc7d33de6400cb5baa49008b20920d5e
Jeremy S. Lee <jeremysanglee@gmail.com>
2017-11-16 14:38:05 -0800

    edit readme and comment for findVertex and other related functions

--- src/models/edge-model.js
+++ src/models/edge-model.js
@@ -35,20 +35,33 @@ class EdgeModel extends Model {
     return this.executeQuery(gremlinStr, callback, true);
   }
 
-  // NOT FULLY TESTED
+  /**
+  * finds the first edge with properties matching props object
+  * @param {object} props Object containing key value pairs of properties
+  * @param {function} callback Some callback function with (err, result) arguments
+  */
   find(props, callback) {
     let gremlinStr = `g.E(${this.getIdFromProps(props)}).hasLabel('${this.label}')` + this.actionBuilder('has', props);
     gremlinStr += ".limit(1)";
     return this.executeOrPass(gremlinStr, callback, true);
   }
 
-  // NOT FULLY TESTED
+  /**
+  * finds the all edges with properties matching props object
+  * @param {object} props Object containing key value pairs of properties
+  * @param {function} callback Some callback function with (err, result) arguments
+  */
   findAll(props, callback) {
     let gremlinStr = `g.E(${this.getIdFromProps(props)}).hasLabel('${this.label}')` + this.actionBuilder('has', props);
     return this.executeOrPass(gremlinStr, callback);
   }
 
-  // NOT YET TESTED
+  /**
+  * finds the all vertices with properties matching props object connected by the relevant edge(s)
+  * @param {object} vertexModel vertexModel that corresponds to the vertex
+  * @param {object} properties Object containing key value pairs of properties to find on vertices
+  * @param {function} callback Some callback function with (err, result) arguments
+  */
   findVertex(vertexModel, properties, callback) {
     let label, props, model;
     if (typeof vertexModel === 'string') {
